76c13779b6d85869b187306e66c5d415832c756d
Dominic <neku113@gmail.com>
2021-06-14 21:04:12 -0400

    Initial Push
    Powershell script that conducts a validation check on a file. Results are printed out, enjoy your Tegrity Weeb!
    (made and created in America)

---
+++ Getchu-Sum-Tegrity.ps1
@@ -0,0 +1,19 @@
+﻿echo Getchu-sum-Tegrity~ 
+
+
+Write-Host In order to use this correctly, you need to be in the same directory as the file being checked! You may also need to run as admin [ set-executionpolicy remotesigned ] to allow scripts. 
+echo - 
+
+# Defines Variable by input of filename ( I copy and paste usually)
+$Filename = Read-Host -Prompt 'Input filename You wish to verify ( make sure it is in current directory w/extension)'
+
+#Checks over $Filename and confirms its hash Value
+$Checked = Get-FileHash .\$Filename | Format-List
+
+#Returns the Value of the file being checked over
+echo - - -
+Write-Host Got your Tegrity: 
+echo $Checked
+
+pause
+
